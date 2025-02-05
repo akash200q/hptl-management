dbcd78d8d2b1aa7a37c127577fed0cd0633f5c52
Amitkumar4592 <amitkumarchowdary2004@gmail.com>
2025-02-05 15:05:34 +0530

    hi

--- hospital/server/mongo.js
+++ hospital/server/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://akash611pd:Aloha123@cluster0.a4uxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+        await mongoose.connect('mongodb+srv://akash611p:akash611p@cluster0.dvsbr.mongodb.net/', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
